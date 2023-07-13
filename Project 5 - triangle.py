d3bc78b946cc547b297207818e1c4223746b4c9d
KiraG2023 <138417595+KiraG2023@users.noreply.github.com>
2023-07-12 20:20:52 -0500

    Update Project 5
    This is a test for commit changes

--- Project 5 - triangle.py
+++ Project 5 - triangle.py
@@ -1,6 +1,6 @@
 print("Enter with base:")
-base = int(input(6))
+base = int(input(20))
 print("Enter with height:")
-height = int(input(5))
+height = int(input(23))
 area = base * height
 print("The area is", area, "square units.")
